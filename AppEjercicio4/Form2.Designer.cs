7c5a4ceb7ba36222e09060a871964c4147400ef6
carloscalderon <carloscalderon@hotmail.com>
2019-11-12 21:07:29 -0500

    CUADRADO DE UN NUMERO

---
+++ AppEjercicio4/Form2.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace AppEjercicio4
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCALCULAR = new System.Windows.Forms.Button();
+            this.btnNUEVO = new System.Windows.Forms.Button();
+            this.txtnumero = new System.Windows.Forms.TextBox();
+            this.txtresultado = new System.Windows.Forms.TextBox();
+            this.btnSALIR = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CALCULAR EL CUADRADO DE UN NUMERO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "INGRESAR °N    :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "RESULTADO      :";
+            // 
+            // btnCALCULAR
+            // 
+            this.btnCALCULAR.Location = new System.Drawing.Point(263, 37);
+            this.btnCALCULAR.Name = "btnCALCULAR";
+            this.btnCALCULAR.Size = new System.Drawing.Size(89, 24);
+            this.btnCALCULAR.TabIndex = 3;
+            this.btnCALCULAR.Text = "CALCULAR";
+            this.btnCALCULAR.UseVisualStyleBackColor = true;
+            this.btnCALCULAR.Click += new System.EventHandler(this.btnCALCULAR_Click);
+            // 
+            // btnNUEVO
+            // 
+            this.btnNUEVO.Location = new System.Drawing.Point(263, 83);
+            this.btnNUEVO.Name = "btnNUEVO";
+            this.btnNUEVO.Size = new System.Drawing.Size(89, 23);
+            this.btnNUEVO.TabIndex = 4;
+            this.btnNUEVO.Text = "NUEVO";
+            this.btnNUEVO.UseVisualStyleBackColor = true;
+            this.btnNUEVO.Click += new System.EventHandler(this.btnNUEVO_Click);
+            // 
+            // txtnumero
+            // 
+            this.txtnumero.Location = new System.Drawing.Point(124, 71);
+            this.txtnumero.Name = "txtnumero";
+            this.txtnumero.Size = new System.Drawing.Size(100, 20);
+            this.txtnumero.TabIndex = 5;
+            // 
+            // txtresultado
+            // 
+            this.txtresultado.Location = new System.Drawing.Point(124, 104);
+            this.txtresultado.Name = "txtresultado";
+            this.txtresultado.Size = new System.Drawing.Size(100, 20);
+            this.txtresultado.TabIndex = 6;
+            // 
+            // btnSALIR
+            // 
+            this.btnSALIR.Location = new System.Drawing.Point(264, 129);
+            this.btnSALIR.Name = "btnSALIR";
+            this.btnSALIR.Size = new System.Drawing.Size(88, 25);
+            this.btnSALIR.TabIndex = 7;
+            this.btnSALIR.Text = "SALIR";
+            this.btnSALIR.UseVisualStyleBackColor = true;
+            this.btnSALIR.Click += new System.EventHandler(this.btnSALIR_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 174);
+            this.Controls.Add(this.btnSALIR);
+            this.Controls.Add(this.txtresultado);
+            this.Controls.Add(this.txtnumero);
+            this.Controls.Add(this.btnNUEVO);
+            this.Controls.Add(this.btnCALCULAR);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "X AL CUADRADO";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCALCULAR;
+        private System.Windows.Forms.Button btnNUEVO;
+        private System.Windows.Forms.TextBox txtnumero;
+        private System.Windows.Forms.TextBox txtresultado;
+        private System.Windows.Forms.Button btnSALIR;
+    }
+}
---
+++ AppEjercicio4/Form2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppEjercicio4
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCALCULAR_Click(object sender, EventArgs e)
+        {
+            int num, resul;
+            num = int.Parse(this.txtnumero.Text);
+
+            resul = num * num;
+            this.txtresultado.Text = resul.ToString();
+
+
+        }
+
+        private void btnNUEVO_Click(object sender, EventArgs e)
+        {
+            this.txtnumero.Text = "";
+            this.txtresultado.Text = "";
+        }
+
+        private void btnSALIR_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
